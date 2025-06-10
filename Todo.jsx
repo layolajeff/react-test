172b72117d7a4353128a51c59a2e98b86c8c3015
layolajeff <layolaj@yahoo.com>
2025-06-10 14:26:10 -0700

    another one

--- Todo.jsx
+++ Todo.jsx
@@ -1,9 +1,13 @@
 function Todo ({title}) {
-    
+    function deleteTodo(id) {
+    console.log('deleteTodo()', title.toUpperCase())
+    }
+
     return (
         <div className="todo">
-        <h2>{title}</h2>
-        <button>Delete</button>
+        <p>{ title }</p>
+        
+        <button onClick = {() => deleteTodo(1)}>Delete</button>
       </div>
     )
 }
